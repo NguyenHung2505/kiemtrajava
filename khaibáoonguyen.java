b8c24f67b2754572d9694fd1b1ecb17685ffe94e
chandoithatsu <100838893+Afdfghjhgcfghvh@users.noreply.github.com>
2022-04-05 16:34:02 +0700

    g

---
+++ khaibáoonguyen.java
@@ -0,0 +1,14 @@
+public class khaibáoonguyen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number");
+        int number = sc.nextInt();
+        if(number<2)
+            System.out.println(number + "is not a premi");
+        else (
+                int i = 2 ;
+                boolean check = true;
+                while ( ( i <= ))
+                )
+    }
+}
